3a347d3316aacdf85229751182576d3feb9654f0
himalay123456 <himalay@solulab.com>
2022-09-19 08:19:10 +0530

    Nextt config updated

--- next.config.js
+++ next.config.js
@@ -3,24 +3,8 @@ const path = require("path");
 module.exports = {
   trailingSlash: true,
   reactStrictMode: false,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   experimental: {
     esmExternals: false,
     jsconfigPaths: true, // enables it for both jsconfig.json and tsconfig.json
   },
-  // webpack: (config) => {
-  //   config.resolve.alias = {
-  //     ...config.resolve.alias,
-  //     apexcharts: path.resolve(
-  //       __dirname,
-  //       "./node_modules/apexcharts-clevision"
-  //     ),
-  //   };
-
-  //   return config;
-  // },
 };
